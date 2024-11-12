2fd0c9d426370bdf5589db5f447fcdf2a82cf725
shagrat164 <dprotsvetov@ya.ru>
2024-11-12 16:43:59 +0300

    HW06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,37 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if in == nil {
+		res := make(Bi)
+		close(res)
+		return res
+	}
+
+	outCh := in
+	for _, stage := range stages {
+		outCh = func(in In) (out Out) {
+			bridgeCh := make(Bi) // Создаётся промежуточный канал для связи стадий
+			go func() {
+				defer close(bridgeCh) // Закрытие канала по завершению работы
+
+				for {
+					select {
+					case <-done: // Прекратить работу при закрытии `done`
+						<-in
+						return
+					case data, ok := <-in:
+						if !ok { // Завершение если вход закрыт
+							return
+						}
+						select {
+						case <-done: // Проверка `done` перед отправкой
+						case bridgeCh <- data:
+						}
+					}
+				}
+			}()
+			return stage(bridgeCh)
+		}(outCh)
+	}
+	return outCh
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -93,6 +93,18 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("in is nil", func(t *testing.T) {
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(nil, nil, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Len(t, result, 0)
+		require.Less(t, int64(elapsed), int64(sleepPerStage)*int64(len(stages)-1)+int64(fault))
+	})
 }
 
 func TestAllStageStop(t *testing.T) {
@@ -150,6 +162,5 @@ func TestAllStageStop(t *testing.T) {
 		wg.Wait()
 
 		require.Len(t, result, 0)
-
 	})
 }
