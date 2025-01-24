e4361986ce688cf6625c5fef905c1735fa5d5cda
shagrat164 <dprotsvetov@ya.ru>
2025-01-24 10:20:48 +0300

    add bench

--- hw10_program_optimization/stats_bench_test.go
+++ hw10_program_optimization/stats_bench_test.go
@@ -2,26 +2,25 @@ package hw10programoptimization
 
 import (
 	"bytes"
-	"fmt"
 	"io"
-	"strings"
 	"testing"
 )
 
-func generateLargeData(n int) string {
-	var builder strings.Builder
-	for i := 0; i < n; i++ {
-		builder.WriteString(`{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"user`)
-		builder.WriteString(fmt.Sprintf("%d", i))
-		builder.WriteString(`@domain.com","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}\n`)
-	}
-	return builder.String()
-}
+var data = `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}
+{"Id":6,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":7,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":8,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":9,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":10,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
 
-// $ go test -benchmem -count 10 -bench=.
+// $ go test -benchmem -count 10 -cpu 2 -bench=.
 func BenchmarkGetDomainStat(b *testing.B) {
-	domain := "domain.com"
-	reader := bytes.NewReader([]byte(generateLargeData(100)))
+	domain := "com"
+	reader := bytes.NewReader([]byte(data))
 
 	for i := 0; i < b.N; i++ {
 		reader.Seek(0, io.SeekStart)
